f66c0742fa5dba75f412d561349ce91297b0fd25
ZeronP <nik4m.5hreyas@gmail.com>
2020-05-18 18:28:21 +0530

    Sample Script

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { useWeb3 } from '@openzeppelin/network/react';
+import Web3Info from './components/Web3Info.js';
+
+
+const infuraProjectId = '31b812de32c94d37b976492466f3e372';
+
 function App() {
+
+  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Network Configurations:</h1>
+        <Web3Info title="Web3 Info" web3Context={web3Context} />
+      </div>
     </div>
   );
 }
